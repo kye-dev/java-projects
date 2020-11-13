94c89f142d6d601620f23af5e4970992f3cc9eed
kyematzen <kyleofficalhd@gmail.com>
2020-11-13 16:03:03 -0500

    Updated spacing before classes between license.

--- Freetime/Data Types/src/com/kyematzen/datatypes/DataTypes.java
+++ Freetime/Data Types/src/com/kyematzen/datatypes/DataTypes.java
@@ -14,6 +14,7 @@ package com.kyematzen.datatypes;
  * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
  * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 public class DataTypes {
 
     // Starting position for compiler program execution
