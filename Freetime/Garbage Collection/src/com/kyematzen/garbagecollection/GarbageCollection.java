cb52fac23cbc5c61595b2e6dd041e0db53e63cee
kyematzen <kyleofficalhd@gmail.com>
2020-11-13 19:16:57 -0500

    Added new project Garbage Collection

---
+++ Freetime/Garbage Collection/src/com/kyematzen/garbagecollection/GarbageCollection.java
@@ -0,0 +1,39 @@
+package com.kyematzen.garbagecollection;
+
+/**
+ * Copyright 2020, Kye Matzen, http://kyematzen.com
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+public class GarbageCollection {
+
+    static class Example {
+
+    }
+
+    // Starting position for compiler program execution
+    public static void main(String[] args) {
+
+        Example exampleObject = new Example();
+
+        // Nullifies the reference variable
+        exampleObject = null;
+
+        // requests for garbage collector
+        System.gc();
+
+        // another way for requesting garbage collector
+        // Runtime.getRuntime().gc();
+
+        // End of compiler program execution
+    }
+}
