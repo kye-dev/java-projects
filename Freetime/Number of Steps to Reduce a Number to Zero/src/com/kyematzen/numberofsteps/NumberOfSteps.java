7f10af232d03975d03113115024e9b5dc6c73115
kyematzen <kyleofficalhd@gmail.com>
2020-11-15 11:22:20 -0500

    Added new project Number of Steps to Reduce a Number to Zero

---
+++ Freetime/Number of Steps to Reduce a Number to Zero/src/com/kyematzen/numberofsteps/NumberOfSteps.java
@@ -0,0 +1,45 @@
+package com.kyematzen.numberofsteps;
+
+/**
+ * Copyright 2020, Kye Matzen, http://kyematzen.com
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+public class NumberOfSteps {
+
+    // Starting position for compiler program execution
+    public static void main(String[] args) {
+
+        // Examples
+        System.out.println("Output: " + numberOfSteps(14));
+        System.out.println("Output: " + numberOfSteps(8));
+        System.out.println("Output: " + numberOfSteps(123));
+
+        // End of compiler program execution
+    }
+
+    public static int numberOfSteps (int num) {
+        int steps = 0;
+
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+}
