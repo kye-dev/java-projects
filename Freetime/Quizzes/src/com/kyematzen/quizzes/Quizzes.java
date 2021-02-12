b5b2ba3d89c0b030cda2af9f1470c8d853b72b35
Kye <kyleofficalhd@gmail.com>
2021-02-11 23:07:08 -0500

    Updated project Quizzes
    In this update, I updated the code and added the ability to grade multiple student quizzes.

--- Freetime/Quizzes/src/com/kyematzen/quizzes/Quizzes.java
+++ Freetime/Quizzes/src/com/kyematzen/quizzes/Quizzes.java
@@ -1,28 +1,74 @@
 package com.kyematzen.quizzes;
 
-import java.util.Arrays;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Quizzes {
 
-    private static int questionsAmount;
-    private String[] questions;
-    //private
+    private static final DecimalFormat decimalFormat = new DecimalFormat("###.##");
+    private static int questionsAmount = 0;
+    private static String[] answerKey;
 
     public static void main(String[] args) {
         // 1. Implement Scanner
         Scanner localScanner = new Scanner(System.in);
 
-//        // 1. Ask the user how many questions
-//        System.out.println("How many questions would you like for the quiz?");
-//        questionsAmount = localScanner.nextInt();
+        gradeQuiz(localScanner);
+    }
 
-        // 2. Ask the user to enter the key.
-        String example = "34 7 13 100 81 3 9 10 321 12";
-        String[] exampleArray = example.split("");
+    public static void gradeQuiz(Scanner localScanner) {
+        // 1. Ask the user how many questions
+        while (questionsAmount == 0) {
+            System.out.println("How many questions would you like for the quiz?");
+            questionsAmount = localScanner.nextInt();
+        }
 
-        System.out.println(Arrays.toString(exampleArray));
+        getQuizAnswers(localScanner);
+    }
 
-        // 34 7 13 100 81 3 9 10 321 12
+    public static void getQuizAnswers(Scanner localScanner) {
+        String answer = "";
+        System.out.println("Please provide quiz key.");
+        while (answer.isEmpty()) {
+            answer = localScanner.nextLine();
+        }
+
+        answerKey = answer.split(" ");
+
+        getStudentAnswers(localScanner);
+    }
+
+    public static void getStudentAnswers(Scanner localScanner) {
+        String answer = "";
+        System.out.println("Please provide student's answers.");
+        while (answer.isEmpty()) {
+            answer = localScanner.nextLine();
+        }
+
+        String[] studentKey = answer.split(" ");
+        int questionsCorrect = 0;
+
+        for (int i = 0; i < answerKey.length; i++) {
+            if (answerKey[i].equals(studentKey[i])) {
+                questionsCorrect++;
+            }
+        }
+
+        System.out.println("\nQuestions Correct: " + questionsCorrect);
+        System.out.println("Percent Correct: " + calculatePercentage(questionsCorrect, questionsAmount) + "%");
+
+        String gradeAgain = "";
+        System.out.println("\nGrade another quiz (y/n)");
+        while (gradeAgain.isEmpty()) {
+            gradeAgain = localScanner.nextLine();
+        }
+
+        if (gradeAgain.equalsIgnoreCase("y")) {
+            getStudentAnswers(localScanner);
+        }
+    }
+
+    private static String calculatePercentage(int questionsCorrect, int questionsAmount) {
+        return decimalFormat.format(( (double) questionsCorrect * 100 / questionsAmount));
     }
 }
